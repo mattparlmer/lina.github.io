7354599d25fff8246482c61e430c5895ba44d436
Lina Lu <linalu001@gmail.com>
2015-10-17 15:19:23 -0700

    added clicking with jquery

--- resources/javascript.js
+++ resources/javascript.js
@@ -1,1 +1,18 @@
+
+$(document).ready(function() {
+  /* Add code from the next steps here */
+  $("#copyright").click(function(){ 
+    $(this).css('color','purple'); 
+  });
+
+});
+
 // Your Javascript will go here!
+
+function replaceName() {
+  var myNewName = window.prompt('Pick a new name!');
+  document.getElementById('user-name').innerHTML = myNewName;
+}
+
+document.onclick = replaceName;
+
