68ba7867cc1a540ff1fc61232ce3d553ae4426ec
Lina Lu <linalu001@gmail.com>
2015-11-10 00:21:59 -0800

    force append

--- resources/javascript.js
+++ resources/javascript.js
@@ -20,7 +20,10 @@ var findBrowser = function() {
 	console.log('what')
 	var getBrowserProps = navigator.userAgent;
 	document.getElementById('browser-props').innerHTML = getBrowserProps;
+	$('head').append('<meta name="apple-itunes-app" content="app-id=960059100, app-argument=standapp://">');
 };
 
 findBrowser();
+
+
 
