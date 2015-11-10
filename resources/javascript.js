0c74ddc2d637a24376b9f5fca1c9a05bf1e8bf2f
Lina Lu <linalu001@gmail.com>
2015-11-10 00:33:20 -0800

    lets try this thing someone else made

--- resources/javascript.js
+++ resources/javascript.js
@@ -17,10 +17,9 @@ function replaceName() {
 // document.onclick = replaceName;
 
 var findBrowser = function() {
-	console.log('what')
 	var getBrowserProps = navigator.userAgent;
 	document.getElementById('browser-props').innerHTML = getBrowserProps;
-	$('head').append('<meta name="apple-itunes-app" content="app-id=960059100, app-argument=standapp://">');
+	// $('head').append('<meta name="apple-itunes-app" content="app-id=960059100, app-argument=standapp://">');
 };
 
 findBrowser();
